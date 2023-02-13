fd4c8312f7ab004756fe737c228b155bc12d8d21
sidao <sidneicarrarojunio@gmail.com>
2023-02-13 10:45:27 -0300

    tutorial manager

---
+++ ProjectA/Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectA.Managers {
+    
+    public class TutorialManager : MonoBehaviour {
+
+        [Serializable]
+        public struct TutorialInfo {
+            public GameObject TutorialText;
+            public float TimeDelay;
+        }
+        
+        public float InitialDelay;
+        public List<TutorialInfo> TutorialTexts = new List<TutorialInfo>();
+
+        private float m_delayBetweenTexts;
+        private int m_currentTextIndex;
+
+        private void Awake() {
+            m_delayBetweenTexts = InitialDelay;
+            StartCoroutine(nameof(ShowTextDelay));
+        }
+
+        private IEnumerator ShowTextDelay() {
+            if (m_currentTextIndex >= TutorialTexts.Count) {
+                yield break;
+            }
+            
+            yield return new WaitForSeconds(m_delayBetweenTexts);
+            HideAndShowNext();
+        }
+
+        private void HideAndShowNext() {
+            TutorialTexts[m_currentTextIndex].TutorialText.SetActive(true);
+            m_delayBetweenTexts = TutorialTexts[m_currentTextIndex].TimeDelay;
+            StartCoroutine(nameof(HideText));
+        }
+        
+        private IEnumerator HideText() {
+            if (m_currentTextIndex >= TutorialTexts.Count) {
+                yield break;
+            }
+            
+            yield return new WaitForSeconds(m_delayBetweenTexts);
+            TutorialTexts[m_currentTextIndex].TutorialText.SetActive(false);
+            m_currentTextIndex += 1;
+            StartCoroutine(nameof(ShowTextDelay));
+        }
+    }
+}
